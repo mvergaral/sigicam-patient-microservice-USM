056900a7f2fca1b639b8eb11c556649b89fec86d
mvergara <matias@poliglota.cl>
2020-07-24 18:33:18 -0400

    Basic setup for microservice implementation

---
+++ Microservice/src/main/java/CalceTeam/Microservice/SigicamPatientsApplication.java
@@ -0,0 +1,13 @@
+package CalceTeam.Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigicamPatientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigicamPatientsApplication.class, args);
+	}
+
+}
---
+++ Microservice/src/test/java/CalceTeam/Microservice/SigicamPatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package CalceTeam.Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigicamPatientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
